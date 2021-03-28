5be4b17499c95fc345175c151fda0c8fd7c0a4eb
Jacek <jacek.dominik.koziej@gmail.com>
2021-03-28 22:55:44 +0200

    added home and champions html

--- lolknow-application/src/main/java/com/jacek/koziej/application/api/ShowChampionsController.java
+++ lolknow-application/src/main/java/com/jacek/koziej/application/api/ShowChampionsController.java
@@ -2,12 +2,12 @@ package com.jacek.koziej.application.api;
 
 import com.jacek.koziej.integration.ChampionsService;
 import com.jacek.koziej.integration.model.Champion;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,12 +17,12 @@ import java.util.Optional;
 @RequestMapping("/v1/champions")
 public class ShowChampionsController {
 
-    private List<Champion> all;
+    private List<Champion> allChampions;
 
     @GetMapping("/all")
     public String allChampions(Model model) {
-        if (all == null)  all = new ChampionsService().getAll();
-        model.addAttribute("champions", all);
+        if (allChampions == null)  allChampions = new ChampionsService().getAll();
+        model.addAttribute("champions", allChampions);
         return "champions";
 
 //        StringBuilder stringBuilder = new StringBuilder();
@@ -37,8 +37,8 @@ public class ShowChampionsController {
 
     @GetMapping("/single")
     public String single(Model model, @RequestParam("id") String id) {
-        if (all == null)  all = new ChampionsService().getAll();
-        Optional<Champion> first = all.stream().filter(champion -> champion.getName().equals(id)).findFirst();
+        if (allChampions == null)  allChampions = new ChampionsService().getAll();
+        Optional<Champion> first = allChampions.stream().filter(champion -> champion.getName().equals(id)).findFirst();
         model.addAttribute("champion", first.get());
         return "champion";
     }
