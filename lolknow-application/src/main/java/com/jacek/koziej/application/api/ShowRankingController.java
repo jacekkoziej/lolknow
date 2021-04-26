4e68b3feb4673784508a3385ae726af5a1a593c7
Jacek <jacek.dominik.koziej@gmail.com>
2021-04-26 21:15:45 +0200

    ranking, champions html revamped

--- lolknow-application/src/main/java/com/jacek/koziej/application/api/ShowRankingController.java
+++ lolknow-application/src/main/java/com/jacek/koziej/application/api/ShowRankingController.java
@@ -30,37 +30,9 @@ public class ShowRankingController {
         this.rankingService = rankingService;
     }
 
-    @GetMapping("/challenger")
-    public String showRanking2() {
-        StringBuilder stringBuilder = new StringBuilder();
-        //creating list and sorting by ranking using stream
-        List<Player> sortedPlayers = rankingService.getAll().stream()
-                .sorted(Comparator.comparing(Player::getLeaguePoints).reversed())
-                .collect(Collectors.toList());
-        //variable used to print position of a player in a ladder
-        AtomicInteger cnt = new AtomicInteger(0);
-
-        sortedPlayers.forEach(player -> stringBuilder
-                .append(player.getSummonerName())
-                .append(", ")
-                .append(player.getLeaguePoints())
-                .append(", W/L = ")
-                .append(player.getWins())
-                .append("/")
-                .append(player.getLosses())
-                .append(", ranking = ")
-                //print ranking position, there is no information about position from service
-                .append(cnt.incrementAndGet())
-                .append("<br>"));
-
-        return stringBuilder.toString();
-    }
-
     @GetMapping("/all")
     public String bestPlayers(Model model) {
-        if (bestPlayers == null) bestPlayers = rankingService
-                .getAll()
-                .stream()
+        if (bestPlayers == null) bestPlayers = rankingService.getAll().stream()
                 .sorted(Comparator.comparing(Player::getLeaguePoints).reversed())
                 .collect(Collectors.toList());
         model.addAttribute("ranking", bestPlayers);
